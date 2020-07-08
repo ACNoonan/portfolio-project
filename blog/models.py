2f9ba9044365de7fe41fe10d67140c37897ec014
Adam Noonan <adamcnoonan@gmail.com>
2020-07-08 13:16:03 -0400

    7/8 Update: Added blog post pages, polished links.

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
     post = models.TextField()
     pubdate = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.post[:100]
+
+    def pubdate_pretty(self):
+        return self.pubdate.strftime('%B %e, %Y')
